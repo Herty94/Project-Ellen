c5a52841f5d5861fe512ef2672332aec58a291c8
Herty94 <da.hertneky@gmail.com>
2020-10-12 22:59:03 +0200

    hammer Controller

--- src/main/java/sk/tuke/kpi/oop/game/Hammer.java
+++ src/main/java/sk/tuke/kpi/oop/game/Hammer.java
@@ -4,11 +4,18 @@ import sk.tuke.kpi.gamelib.framework.AbstractActor;
 import sk.tuke.kpi.gamelib.graphics.Animation;
 
 public class Hammer extends AbstractActor {
+    private int useNum;
     public Hammer() {
         setAnimation(new Animation("sprites/hammer.png"));
-
+        this.useNum = 3;
     }
-    public int use() {
-        return 0;
+    public boolean use() {
+        if(this.useNum>0)
+            useNum--;
+        if(this.useNum<=0) {
+            (getScene()).removeActor(this);
+            return false;
+        }
+        return true;
     }
 }
--- src/main/java/sk/tuke/kpi/oop/game/Reactor.java
+++ src/main/java/sk/tuke/kpi/oop/game/Reactor.java
@@ -9,6 +9,7 @@ public class Reactor extends AbstractActor {
     private Animation normalAnimation;
     private Animation redAnimation;
     private Animation destroyedAnimation;
+    private Animation offAnimation;
     private boolean state;
 
     public Reactor(){
@@ -17,7 +18,9 @@ public class Reactor extends AbstractActor {
         this.normalAnimation = new Animation("sprites/reactor_on.png", 80, 80, 0.15f, Animation.PlayMode.LOOP_PINGPONG);
         this.redAnimation = new Animation("sprites/reactor_hot.png", 80, 80, 0.15f, Animation.PlayMode.LOOP_PINGPONG);
         this.destroyedAnimation = new Animation("sprites/reactor_broken.png", 80, 80, 0.1f, Animation.PlayMode.LOOP);
-        setAnimation(normalAnimation);
+        this.offAnimation = new Animation("sprites/reactor.png");
+        setAnimation(offAnimation);
+        this.state = false;
     }
 
     public int getTemperature() {
@@ -27,17 +30,20 @@ public class Reactor extends AbstractActor {
         return this.damage;
     }
     public void increaseTemperature(int increment){
+        if(!this.state)
+            return;
         if(increment<0)
             return;
         this.temperature+=increment;
-        if(this.temperature>6000)
-            this.temperature=6000;
+
         if(this.damage>=33&& this.damage<=66)
             this.temperature+= Math.ceil(increment*0.5);
         else if(this.damage>66)
             this.temperature+= increment;
 
         int old_damage=this.damage;
+        if(this.temperature>6000)
+            this.temperature=6000;
         if(this.temperature>=2000)
             this.damage = (int)Math.ceil(100.0/4000.0*(this.temperature - 2000));
         if(this.damage<old_damage)
@@ -45,6 +51,8 @@ public class Reactor extends AbstractActor {
         updateAnimation();
     }
     public void decreaseTemperature(int decrement){
+        if(!this.state)
+            return;
         if(decrement<0)
             return;
         if(this.damage==100)
@@ -55,13 +63,30 @@ public class Reactor extends AbstractActor {
             this.temperature-=Math.ceil(decrement*0.5);
         updateAnimation();
     }
+    public boolean isRunning(){
+        return this.state;
+    }
     public void turnOff(){
-
+        this.state= false;
+        updateAnimation();
     }
     public void turnOn(){
-
+        this.state = true;
+        updateAnimation();
+    }
+    public void repairWith(Hammer hammer){
+        if(hammer == null || this.damage<=0 || !hammer.use())
+            return;
+        if(this.damage<=50)
+            this.damage= 0;
+        else
+            this.damage-=50;
     }
     private void updateAnimation(){
+        if(!this.state){
+            setAnimation(offAnimation);
+            return;
+        }
         float frameRate = 0.15f-(0.12f/100.0f*this.damage);
         if(this.damage>20) {
             //  redAnimation.setFrameDuration();
@@ -73,7 +98,10 @@ public class Reactor extends AbstractActor {
             setAnimation(this.redAnimation);
         else
             setAnimation(this.normalAnimation);
-        if(this.temperature>=6000)
+        if(this.temperature>=6000) {
             setAnimation(this.destroyedAnimation);
+            this.state = false;
+        }
+
     }
 }
