ef6425bc6fae461aa44dfc82cb8445b43011b7bc
Herty94 <da.hertneky@gmail.com>
2020-10-30 20:32:53 +0100

    Teleport

---
+++ src/main/java/sk/tuke/kpi/oop/game/Teleport.java
@@ -0,0 +1,63 @@
+package sk.tuke.kpi.oop.game;
+
+import org.jetbrains.annotations.NotNull;
+import sk.tuke.kpi.gamelib.Disposable;
+import sk.tuke.kpi.gamelib.Scene;
+import sk.tuke.kpi.gamelib.actions.Invoke;
+import sk.tuke.kpi.gamelib.actions.When;
+import sk.tuke.kpi.gamelib.framework.AbstractActor;
+import sk.tuke.kpi.gamelib.framework.Player;
+import sk.tuke.kpi.gamelib.framework.actions.Loop;
+import sk.tuke.kpi.gamelib.graphics.Animation;
+
+import java.util.Objects;
+
+public class Teleport extends AbstractActor {
+    private Teleport destinationTeleport;
+    private Player player;
+    private boolean something;
+    private Disposable disposable;
+    public Teleport(Teleport teleport){
+        this.destinationTeleport = teleport;
+        setAnimation(new Animation("sprites/lift.png"));
+        this.something=false;
+    }
+    public void setBool(Boolean bool){
+        this.something=bool;
+    }
+    public Teleport getDestination(){
+        return this.destinationTeleport;
+    }
+    public void setDestination(Teleport destinationTeleport){
+        if(this!=destinationTeleport)
+        this.destinationTeleport = destinationTeleport;
+        if(disposable!=null)
+            disposable.dispose();
+        addedToScene(getScene());
+    }
+    private void teleportPlayer(Player player){
+
+        player.setPosition(destinationTeleport.getPosX()+(this.getWidth()/2)-(player.getWidth()/2), destinationTeleport.getPosY()+(this.getWidth()/2)-(player.getWidth()/2));
+        destinationTeleport.setBool(false);
+    }
+
+    @Override
+    public void addedToScene(@NotNull Scene scene) {
+        super.addedToScene(scene);
+        this.player = (Player) Objects.requireNonNull(getScene()).getFirstActorByName("Player");
+        if(destinationTeleport !=null)
+          disposable = new Loop<>(new When<>(
+                () -> (player!=null&&playerIntersection()&&something),
+                new Invoke<>(this::teleportPlayer)
+                )).scheduleFor(player);
+    }
+    private boolean playerIntersection(){
+        int px = player.getPosX()+player.getWidth()/2;
+        int py = player.getPosY()+ player.getWidth()/2;
+
+            if(px<this.getPosX()|| px>(this.getPosX()+this.getWidth())||py<this.getPosY()|| py>(this.getPosY()+this.getWidth())) {
+                something = true;
+            }
+        return px >= this.getPosX() && px <= (this.getPosX() + this.getWidth()) && py >= this.getPosY() && py <= (this.getPosY() + this.getWidth());
+    }
+}
