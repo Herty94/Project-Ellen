bb0bdfe2a1a93dd6532f9d41d149a66d9e4c5ccf
Herty94 <da.hertneky@gmail.com>
2020-12-02 18:58:33 +0100

    maps

---
+++ src/main/java/sk/tuke/kpi/oop/game/characters/Health.java
@@ -0,0 +1,41 @@
+package sk.tuke.kpi.oop.game.characters;
+
+public class Health {
+    private int max;
+    private int value;
+    public Health(int health, int max){
+        this.value=health;
+        this.max=max;
+    }
+    @FunctionalInterface
+    public interface ExhaustionEffect {
+        void apply();
+    }
+    public Health(int health){
+        this.value=health;
+        this.max=health;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void refill(int amount){
+        this.value += amount;
+        if(value>max)
+            this.value = this.max;
+    }
+    public void restore(){
+        this.value=this.max;
+    }
+    public void drain(int amount){
+        this.value -=amount;
+        if(amount<0)
+            this.value=0;
+    }
+
+
+    public void onExhaustion(ExhaustionEffect effect){
+        
+    }
+}
