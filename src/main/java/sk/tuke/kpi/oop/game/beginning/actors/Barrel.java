554f6880ad4ff3b5de94ec12976f74686c745180
Herty94 <da.hertneky@gmail.com>
2020-12-12 12:33:21 +0100

    my game

---
+++ src/main/java/sk/tuke/kpi/oop/game/beginning/actors/Barrel.java
@@ -0,0 +1,36 @@
+package sk.tuke.kpi.oop.game.beginning.actors;
+
+import org.jetbrains.annotations.NotNull;
+import sk.tuke.kpi.gamelib.Scene;
+import sk.tuke.kpi.gamelib.framework.AbstractActor;
+import sk.tuke.kpi.gamelib.graphics.Animation;
+import sk.tuke.kpi.gamelib.map.MapTile;
+
+public class Barrel extends AbstractActor implements Pushable {
+
+    private MapTile tile;
+    private Scene scene;
+    public Barrel(){
+        setAnimation(new Animation("sprites/barrel.png"));
+    }
+
+    @Override
+    public void addedToScene(@NotNull Scene scene) {
+        super.addedToScene(scene);
+        this.scene=scene;
+        setTiles();
+    }
+
+    @Override
+    public void setTiles() {
+        if(scene==null)
+            return;
+        tile = scene.getMap().getTile(this.getPosX()/16,this.getPosY()/16);
+        tile.setType(MapTile.Type.WALL);
+    }
+
+    @Override
+    public void removeTile() {
+        tile.setType(MapTile.Type.CLEAR);
+    }
+}
